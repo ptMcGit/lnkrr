7049ce74f23851942728c2a7cf35096546e5d83c
Zach H <zdhadgraft@gtcc.edu>
2016-06-09 18:45:32 -0400

    improvements to w_console.rb

--- w_console.rb
+++ w_console.rb
@@ -1,12 +1,17 @@
 require 'pry'
 require 'httparty'
 
-def my_url arg
-  Url + arg
+def get_request path, auth="webconsole"
+  HTTParty.get Url + path, headers: { "Authorization" => auth }
+end
+
+def post_request path, auth="webconsole"
+  HTTParty.post Url + path, headers: { "Authorization" => auth }
 end
 
 Heroku  = "https://lnkrr.herokuapp.com"
 Local   = "http://localhost:4567"
 Url     = Local
-# HTTParty.get my_url ...
+# get_request PATH [AUTHORIZATION_NAME]
+# post_request PATH [AUTHORIZATION_NAME]
 binding.pry
