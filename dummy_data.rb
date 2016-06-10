016dd097ee1c2e42c6b1a5ca6709f4d0e0eb8b9c
Zach H <zdhadgraft@gtcc.edu>
2016-06-09 23:19:10 -0400

    Created dummy data helper functions.

---
+++ dummy_data.rb
@@ -0,0 +1,85 @@
+# users
+
+def luke
+  {
+      username: "skydaddy",
+      password: "lightsaber",
+      first_name: "Luke",
+      last_name: "Skywalker",
+      location: "Tatooine",
+      joined_date: "2016-04-05"
+  }
+end
+
+def luke
+  {
+      username: "skydaddy",
+      password: "deathchoke",
+      first_name: "Darth",
+      last_name: "Vader",
+      location: "the Death Star",
+      joined_date: "2016-04-05"
+  }
+end
+
+def storm_trooper1
+  {
+      username: "rad_Steve",
+      password: "cool123",
+      first_name: "Steve",
+      last_name: "Miller",
+      location: "the Death Star",
+      joined_date: "2016-04-05"
+  }
+end
+
+def storm_trooper2
+  {
+      username: "swellPhil",
+      password: "dat77",
+      first_name: "Phil",
+      last_name: "Winters",
+      location: "the Death Star",
+      joined_date: "2016-02-18"
+  }
+end
+
+def storm_trooper3
+  {
+      username: "MojoMike",
+      password: "atat65",
+      first_name: "Mike",
+      last_name: "Storm",
+      location: "The Death Star",
+      joined_date: "2015-12-28"
+  }
+end
+
+# Links
+
+def github
+  {
+      title: "GitHub",
+      url: "http://www.github.com",
+      description: "Site for sharing software",
+      timestamp: "2016-06-01 05:45:01"
+  }
+end
+
+def apple
+  {
+      title: "Apple Computers",
+      url: "http://www.apple.com",
+      description: "Apple computer company website",
+      timestamp: "2016-02-05 03:45:01"
+  }
+end
+
+def wikipedia
+  {
+    title: "Wikipedia",
+    url: "http://www.wikipedia.org",
+    description: "Free information",
+    timestamp: "2016-02-05 12:50:22"
+  }
+end
