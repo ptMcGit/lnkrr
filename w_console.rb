6c3b2d26c0bda9e1151211005ca9d3b3dbb3aa65
Zach H <zdhadgraft@gtcc.edu>
2016-06-09 23:39:04 -0400

    Improvements to bash_aliases and w_console.rb.

--- w_console.rb
+++ w_console.rb
@@ -1,14 +1,15 @@
 require 'pry'
 require 'httparty'
+require 'json'
 require "./db/setup"
 require "./lib/all"
 
-def get_request path, auth="webconsole"
-  HTTParty.get Url + path, headers: { "Authorization" => auth }
+def get_request path, auth: "webconsole", body: {}
+  HTTParty.get Url + path, headers: { "Authorization" => auth }, body: body.to_json
 end
 
-def post_request path, auth="webconsole"
-  HTTParty.post Url + path, headers: { "Authorization" => auth }
+def post_request path, auth: "webconsole", body: {}
+  HTTParty.post Url + path, headers: { "Authorization" => auth }, body: body.to_json
 end
 
 Heroku  = "https://lnkrr.herokuapp.com"
