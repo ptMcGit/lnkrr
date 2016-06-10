4a3f0607a9bc9be49f5b62d271dc5eeb5800bf40
tythompson <tythompson@gmail.com>
2016-06-10 04:48:12 -0400

    invalid payload but close

--- w_console.rb
+++ w_console.rb
@@ -12,6 +12,15 @@ def post_request path, auth: "webconsole", body: {}
   HTTParty.post Url + path, headers: { "Authorization" => auth }, body: body.to_json
 end
 
+def test_chat
+  token = ENV["SLACK_PAYLOAD"]
+  HTTParty.post(
+      "#{token}",
+      headers: {'content-type' => 'application/json'},
+      body: { "text" => "test text","channel" => "@tythompson","username" => "lnkrrbot","icon_emoji" => ":computer:"}
+      )
+end
+
 Heroku  = "https://lnkrr.herokuapp.com"
 Local   = "http://localhost:4567"
 Url     = Local
