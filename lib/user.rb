8c695266009b43667255f63bc0ee11d5f9dd4f6b
Zach H <zdhadgraft@gtcc.edu>
2016-06-11 20:31:45 -0400

    Fixe User class.

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  validates_presence_of :username, :password
-  validates_length_of :password, minimum: 4
+  validates_presence_of :username
   validates_uniqueness_of :username
 
   has_many :slinks
